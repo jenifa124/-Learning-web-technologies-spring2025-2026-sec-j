ed902ead29311accf17052273a6d0e507da2cf5e
Most.Jenifa Akter <114852184+jenifa124@users.noreply.github.com>
2026-04-19 16:54:15 +0600

    homepage

---
+++ finallab2/A/home.php
@@ -0,0 +1,44 @@
+<?php include '../auth.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Public Home</title>
+</head>
+<body>
+
+<table border="1" width="800" align="center" cellspacing="0" cellpadding="0">
+    
+    <tr>
+        <td style="padding:20px;">
+            <table width="100%">
+                <tr>
+                    <td>
+                        <span style="font-size:42px; color:green; font-weight:bold;">X</span>
+                        <span style="font-size:26px; font-weight:bold;">Company</span>
+                    </td>
+                    <td align="right">
+                        <a href="../A/home.php">Home</a> |
+                        <a href="../C/login.php">Login</a> |
+                        <a href="../B/registration.php">Registration</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+    <tr>
+        <td style="height:150px; padding:20px; vertical-align:top;">
+            <h2 style="margin-top:20px;">Welcome to xCompany</h2>
+        </td>
+    </tr>
+
+    <tr>
+        <td align="center" style="padding:10px;">
+            Copyright &copy; 2017
+        </td>
+    </tr>
+
+</table>
+
+</body>
+</html>
