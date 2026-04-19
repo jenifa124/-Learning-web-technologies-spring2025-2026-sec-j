447bb638e9d983c4d5fa82d88e26742705ce95db
Most.Jenifa Akter <114852184+jenifa124@users.noreply.github.com>
2026-04-19 17:35:18 +0600

    passwordpage

---
+++ finallab2/D/forgot_password.php
@@ -0,0 +1,47 @@
+<?php include '../auth.php'; ?>
+<!DOCTYPE html>
+<html>
+<head><title>Forgot Password</title></head>
+<body>
+
+<table border="1" width="800" align="center" cellspacing="0" cellpadding="0">
+
+<tr>
+<td style="padding:20px;">
+<table width="100%">
+<tr>
+<td>
+<span style="font-size:40px; color:green; font-weight:bold;">X</span>
+<span style="font-size:25px; font-weight:bold;">Company</span>
+</td>
+<td align="right">
+<a href="../A/home.php">Home</a> |
+<a href="../C/login.php">Login</a> |
+<a href="../B/registration.php">Registration</a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+
+<tr>
+<td style="padding:50px;">
+<fieldset style="width:300px; margin:auto;">
+<legend><b>FORGOT PASSWORD</b></legend>
+
+<form method="post">
+Enter Email: <input type="text" name="email"><br><br>
+<input type="submit" value="Submit">
+</form>
+
+</fieldset>
+</td>
+</tr>
+
+<tr>
+<td align="center">Copyright &copy; 2017</td>
+</tr>
+
+</table>
+</body>
+</html>
--- finallab2/auth.php
+++ finallab2/auth.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
 
-$_SESSION['users']['jenifa'] = [
-    'name' => 'jenifa',
-    'password' => '123'
-];
-$_SESSION['logged_in'] = 'jenifa';
-?>
+
+if (!isset($_SESSION['users'])) {
+    $_SESSION['users'] = [];
+}
+
+if (!isset($_SESSION['users']['jenifa'])) {
+    $_SESSION['users']['jenifa'] = [
+        'name' => 'jenifa',
+        'password' => '123'
+    ];
+}
+
+
+if (!isset($_SESSION['logged_in'])) {
+    $_SESSION['logged_in'] = 'jenifa';
+}
